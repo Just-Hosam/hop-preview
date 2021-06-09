584ad31d8ffce4a6dcf1428b181c443dfbee3a84
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-06-09 12:22:08 -0600

    Introduction component setup and styling

---
+++ src/components/Header/Introduction.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Introduction = () => {
+	return (
+		<div id="introduction">
+			<h2>
+				<span>H</span>ELPING <span>O</span>THERS <span>P</span>ROSPER
+			</h2>
+
+			<p>Connecting local businesses to the Web</p>
+		</div>
+	);
+};
+
+export default Introduction;
